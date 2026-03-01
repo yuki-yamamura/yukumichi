34f9d17130c77f7e531d35f846332b25a4150e9c
Yuki Yamamura <126521201+yuki-yamamura@users.noreply.github.com>
2026-03-01 14:45:22 +0900

    chore: ignore OpenNext caches

--- apps/web/eslint.config.mjs
+++ apps/web/eslint.config.mjs
@@ -5,10 +5,9 @@ import nextTs from "eslint-config-next/typescript";
 const eslintConfig = defineConfig([
   ...nextVitals,
   ...nextTs,
-  // Override default ignores of eslint-config-next.
   globalIgnores([
-    // Default ignores of eslint-config-next:
     ".next/**",
+    ".open-next/**",
     "out/**",
     "build/**",
     "next-env.d.ts",
