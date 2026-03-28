46d6f4c688cd205d399c6bdd7fe86d4f7202dbf7
Yuki Yamamura <126521201+yuki-yamamura@users.noreply.github.com>
2026-03-29 07:44:39 +0900

    chore: rename `helpers` directory to `fixtures` in frontend as well

--- apps/web/src/features/spot/components/spot-name-hint/presenter.test.tsx
+++ apps/web/src/features/spot/components/spot-name-hint/presenter.test.tsx
@@ -1,7 +1,7 @@
 import { render, screen } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 
-import { createSpot } from "@/test/helpers/spot";
+import { createSpot } from "@/test/fixtures/spot";
 
 import { SpotNameHintPresenter } from "./presenter";
 
