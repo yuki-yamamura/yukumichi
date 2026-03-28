78fe08e4abc0ac814c5fc04e668ae50648ff4901
Yuki Yamamura <126521201+yuki-yamamura@users.noreply.github.com>
2026-03-28 16:45:12 +0900

    chore: use the same syntax as api for alias

--- apps/api/vitest.config.ts
+++ apps/api/vitest.config.ts
@@ -1,3 +1,5 @@
+import { fileURLToPath } from "node:url";
+
 import { defineConfig } from "vitest/config";
 
 export default defineConfig({
@@ -6,7 +8,7 @@ export default defineConfig({
   },
   resolve: {
     alias: {
-      "@": "./src",
+      "@": fileURLToPath(new URL("src", import.meta.url)),
     },
   },
 });
