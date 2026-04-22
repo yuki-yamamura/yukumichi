529c0a3951017ce9e992cac36b8dfc213441ee34
Yuki Yamamura <126521201+yuki-yamamura@users.noreply.github.com>
2026-04-23 05:58:57 +0900

    fix: fix knip error

--- apps/web/src/libs/zod/custom-issue.ts
+++ apps/web/src/libs/zod/custom-issue.ts
@@ -4,7 +4,7 @@ export const customIssueParamsSchema = z.discriminatedUnion("kind", [
   z.object({ kind: z.literal("invalid_float") }),
 ]);
 
-export type CustomIssueParams = z.infer<typeof customIssueParamsSchema>;
+type CustomIssueParams = z.infer<typeof customIssueParamsSchema>;
 
 export function toCustomIssueParams<const T extends CustomIssueParams>(params: T): T {
   return params;
